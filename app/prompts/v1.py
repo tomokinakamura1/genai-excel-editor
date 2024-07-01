48996027a2984f0173e6776bbcd64de8f8ce1858
Niranjan Khedkar <Niranjan.Khedkar@ibm.com>
2024-07-01 13:58:25 +0530

    version 1 completed

--- app/prompts/v1.py
+++ app/prompts/v1.py
@@ -1,34 +1,26 @@
 prompt_template = """
 <|begin_of_text|><|start_header_id|>system<|end_header_id|>
-ROLE: You're an AI language model, specializing in enhancing and enriching content by providing lengthy,
-detailed descriptions and creative content with minimal information provided.
+ROLE: You're an AI language model, specializing in extracting things from the given dataframe and answering questions relatated to them
 Your responses must strictly adhere to the format and rules specified.
 
 <|eot_id|><|start_header_id|>user<|end_header_id|>
-Here's the metadata for the information you need to use to leverage generating content:
+Here's the input prompt given from the user please adhere to the instructions given in this prompt
 
-{user_input_metadata}
+{input_prompt}
 
 INSTRUCTIONS:
-1. Use the input JSON metadata information and construct answers for the question provided.
-2. Your output must be exclusively in JSON format. Do not include any text other than the JSON output.
-3. Append the <end> token after the JSON output.
-4. Your JSON output should contain two keys: content_key and content_value.
-5. content_key should reflect the original key, while content_value should contain a more detailed and descriptive version of the original value.
+1. Use the input Dataframe metadata information and construct answers for the question provided.
+2. Your output must be exclusively in Dataframe format. Do not include any text other than the Dataframe output.
+3. Append the <end> token after the Dataframe output.
 
 RULES:
-1. Strictly maintain the original keys; do not generate or assume new keys.
-2. Enhance the descriptions while staying true to the context provided by the original values.
-3. Be precise and avoid introducing factual inaccuracies or assumptions not supported by the input data.
-4. Ensure the descriptions are coherent, detailed, and creatively enriched to provide a comprehensive understanding.
+1. Enhance the descriptions while staying true to the context provided by the original values.
+2. Be precise and avoid introducing factual inaccuracies or assumptions not supported by the input data.
+3. Ensure the descriptions are coherent, detailed, and creatively enriched to provide a comprehensive understanding.
 
-Below are examples of the input and desired output formats:
-Input JSON:{{metadata}}
 
-Output JSON: {meta_data_info}
-
-Give output JSON for the following input keeping in mind the Role, Instructions & Rules.
-Input JSON: {question}
-Output JSON (containing answers for all the keys in Input JSON): 
+Give output for the following input keeping in mind the Role, Instructions & Rules.
+Input Dataframe: {df}
+Output Dataframe (containing answers for all the keys in Input Dataframe): 
 <|eot_id|><|start_header_id|>assistant<|end_header_id|>
 """
--- gradio-ui.py
+++ gradio-ui.py
@@ -18,32 +18,40 @@ def download_content(df):
 with gr.Blocks(theme=gr.themes.Soft(), css=CUSTOM_CSS) as demo:
     gr.Image(BANNER_PATH, show_label=False)
     
+    ##heading###
     gr.Markdown(
         "<center><span style='font-size: 26px; font-weight: bold;'>Entity extractor</span></center>") 
     
+    ###input file #####
     file_upload = gr.File(label="Upload Input Excel")
     analyze_button = gr.Button("Generate", elem_id="custom-button1-id",
                                        elem_classes=["custom-button1-class"])
     
     input_table = gr.DataFrame(headers=None)
 
+    ####Entity extraction#######
     prompt=gr.TextArea(label="input_prompt")
 
     
-    
+    output_button = gr.Button("Extract Entity", elem_id="custom-button1-id",
+                                       elem_classes=["custom-button1-class"])
     
     gr.Markdown(
         "<left><span style='font-size: 26px; font-weight: bold;'>Generated Output</span></left>")
     
     output=gr.TextArea(label="Generated Output")
     
+    output_table=gr.DataFrame(headers=None)
     
     
     analyze_button.click(extract_file.read_table, inputs=file_upload, outputs=input_table)
     
+    output_button.click(extract_file.extract_entity, inputs=[prompt,file_upload], outputs=output)
+
+
     download_button = gr.Button("Download Content", elem_id="custom-button1-id",
                                        elem_classes=["custom-button1-class"])    
-    download_button.click(download_content, inputs=input_table, outputs=gr.File())
+    download_button.click(download_content, inputs=output_table, outputs=gr.File())
 
 
 demo.launch(server_port=8080)
