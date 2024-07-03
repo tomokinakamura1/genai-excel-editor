b60ae948e91ec9838681797c1e30b4c7dd24d808
Niranjan Khedkar <Niranjan.Khedkar@ibm.com>
2024-07-03 17:25:16 +0530

    added japanese ui part too

--- gradio-ui_jp.py
+++ gradio-ui_jp.py
@@ -23,34 +23,36 @@ with gr.Blocks(theme=gr.themes.Soft(), css=CUSTOM_CSS) as demo:
         "<center><span style='font-size: 26px; font-weight: bold;'>Entity extractor</span></center>") 
     
     ###input file #####
-    file_upload = gr.File(label="Upload Input Excel")
-    review_button = gr.Button("Review", elem_id="custom-button1-id",
+    file_upload = gr.File(label="入力CSVをアップロード")
+    review_button = gr.Button("総説", elem_id="custom-button1-id",
                                        elem_classes=["custom-button1-class"])
     
     input_table = gr.DataFrame(headers=None)
 
     ####Entity extraction#######
-    prompt=gr.TextArea(label="input_prompt",placeholder="please add underscore instead of spaces in column names")
+    prompt=gr.TextArea(label="入力プロンプト",placeholder="please add underscore instead of spaces in column names")
 
     
-    extract_entity = gr.Button("Extract Entities", elem_id="custom-button1-id",
+    extract_entity = gr.Button("抽出する", elem_id="custom-button1-id",
                                        elem_classes=["custom-button1-class"])
     
     gr.Markdown(
-        "<left><span style='font-size: 26px; font-weight: bold;'>Generated Output</span></left>")
+        "<left><span style='font-size: 26px; font-weight: bold;'>生成された出力</span></left>")
     
-    output=gr.TextArea(label="Generated Output")
+    output=gr.TextArea(label="生成された出力")
     
     output_table=gr.DataFrame(headers=None)
     
+    ###Button operations ####
     
     review_button.click(extract_file.read_table, inputs=file_upload, outputs=input_table)
     
     extract_entity.click(extract_file.extract_entity, inputs=[prompt,file_upload], outputs=[output,output_table])
 
-
-    download_button = gr.Button("Download Content", elem_id="custom-button1-id",
+    ####Download Utils ####
+    download_button = gr.Button("ダウンロード", elem_id="custom-button1-id",
                                        elem_classes=["custom-button1-class"])    
+    
     download_button.click(download_content, inputs=output_table, outputs=gr.File())
 
 
