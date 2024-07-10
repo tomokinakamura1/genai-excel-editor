5c5050e0a8d9fcc81efbc5242cd5f4c4d8a86f22
Niranjan Khedkar <Niranjan.Khedkar@ibm.com>
2024-07-10 09:58:08 +0530

    added line by line yet to fix the json->df part

--- app_jp/utils/extract_file.py
+++ app_jp/utils/extract_file.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from app.prompts import v1
 import json
+import re
 
 load_dotenv()
 
@@ -67,10 +68,51 @@ def read_table(file_path):
     return df
 
 
-def extract_entity(input_prompt,file_path):
+def extract_entity(prompt,column_output,input_table):
     # df = read_table(file_path)
-    df=file_path
-    output=entity(input_prompt,df)
+    df=column_output
+    output=entity(prompt,df)
     output_df=convert_to_df(output)
-    final_df=pd.concat([df, output_df], axis=1)
+    final_df=pd.concat([input_table, output_df], axis=1)
     return output,final_df
+
+
+def extract_entity_line_by_line(input_prompt,file_path):
+    df=file_path
+    # output=entity(input_prompt,df)
+    final_response=[]
+    for index, row in df.iterrows():
+        prompt = append_question(
+            input_prompt=input_prompt,
+            df=row
+        )
+        response=model.generate(prompt)
+        intermediate_respoonse=response.get("results")[0].get("generated_text")[:-5]
+        # print(response)
+        print(intermediate_respoonse)
+        final_response.append(intermediate_respoonse)
+        yield final_response
+    
+    
+
+# Function to extract JSON content
+def extract_json_from_string(json_str):
+    match = re.search(r'{.*}', json_str, re.DOTALL)
+    if match:
+        return match.group(0)
+    return None
+
+def get_table(final_response,file_path):
+    print(final_response)
+    combined_json = []
+
+    for json_str in final_response:
+        json_content = extract_json_from_string(json_str)
+        if json_content:
+            json_obj = json.loads(json_content)
+            combined_json.append(json_obj)
+
+    combined_json_str = json.dumps(combined_json, indent=2, ensure_ascii=False)
+    df=convert_to_df(combined_json_str)
+    final_df=pd.concat([file_path, df], axis=1)
+    return final_df
--- gradio-ui_jp.py
+++ gradio-ui_jp.py
@@ -31,49 +31,97 @@ def get_column_names(file):
 with gr.Blocks(theme=gr.themes.Soft(), css=CUSTOM_CSS) as demo:
     gr.Image(BANNER_PATH, show_label=False)
     
-    ##Header###
-    gr.Markdown(
-        "<center><span style='font-size: 26px; font-weight: bold;'>Entity extractor</span></center>") 
-    
-    ###input file#####
-    file_upload = gr.File(label="入力CSVをアップロード")
-    column_dropdown = gr.Dropdown(label="Select a column to display", choices=[], interactive=True)
-    input_table = gr.DataFrame(headers=None)
+    with gr.Tab("Full processor"):
+        ##Header###
+        gr.Markdown(
+            "<center><span style='font-size: 26px; font-weight: bold;'>Entity extractor</span></center>") 
+        
+        ###input file#####
+        file_upload = gr.File(label="入力CSVをアップロード")
+        column_dropdown = gr.Dropdown(label="Select a column to display", choices=[], interactive=True)
+        input_table = gr.DataFrame(headers=None)
 
-    column_output = gr.DataFrame(label="Selected Column")
-    review_button = gr.Button("総説", elem_id="custom-button1-id",
-                                       elem_classes=["custom-button1-class"])
-    
-    file_upload.change(fn=lambda file: gr.update(choices=get_column_names(file)), inputs=file_upload, outputs=column_dropdown)
+        column_output = gr.DataFrame(label="Selected Column")
+        review_button = gr.Button("総説", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])
+        
+        file_upload.change(fn=lambda file: gr.update(choices=get_column_names(file)), inputs=file_upload, outputs=column_dropdown)
 
-    
-    review_button.click(fn=display_column, inputs=[file_upload, column_dropdown], outputs=[input_table, column_output])
+        
+        review_button.click(fn=display_column, inputs=[file_upload, column_dropdown], outputs=[input_table, column_output])
 
-    ####Entity extraction#######
-    prompt=gr.TextArea(label="入力プロンプト",placeholder="please add underscore instead of spaces in column names")
+        ####Entity extraction#######
+        prompt=gr.TextArea(label="入力プロンプト",placeholder="please add underscore instead of spaces in column names")
 
-    
-    extract_entity = gr.Button("抽出する", elem_id="custom-button1-id",
-                                       elem_classes=["custom-button1-class"])
-    
-    gr.Markdown(
-        "<left><span style='font-size: 26px; font-weight: bold;'>生成された出力</span></left>")
-    
-    output=gr.TextArea(label="生成された出力")
-    
-    output_table=gr.DataFrame(headers=None)
-    
-    ###Button operations ####
-    
-    review_button.click(extract_file.read_table, inputs=file_upload, outputs=input_table)
-    
-    extract_entity.click(extract_file.extract_entity, inputs=[prompt,column_output], outputs=[output,output_table])
+        
+        extract_entity = gr.Button("抽出する", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])
+        
+        gr.Markdown(
+            "<left><span style='font-size: 26px; font-weight: bold;'>生成された出力</span></left>")
+        
+        output=gr.TextArea(label="生成された出力")
+        
+        output_table=gr.DataFrame(headers=None)
+        
+        ###Button operations ####
+        
+        review_button.click(extract_file.read_table, inputs=file_upload, outputs=input_table)
+        
+        extract_entity.click(extract_file.extract_entity, inputs=[prompt,column_output,input_table], outputs=[output,output_table])
 
-    ####Download Utils ####
-    download_button = gr.Button("ダウンロード", elem_id="custom-button1-id",
-                                       elem_classes=["custom-button1-class"])    
-    
-    download_button.click(download_content, inputs=output_table, outputs=gr.File())
+        ####Download Utils ####
+        download_button = gr.Button("ダウンロード", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])    
+        
+        download_button.click(download_content, inputs=output_table, outputs=gr.File())
 
+    with gr.Tab("Line by line processor"):
+        ##Header###
+        gr.Markdown(
+            "<center><span style='font-size: 26px; font-weight: bold;'>Entity extractor</span></center>") 
+        
+        ###input file#####
+        file_upload = gr.File(label="入力CSVをアップロード")
+        column_dropdown = gr.Dropdown(label="Select a column to display", choices=[], interactive=True)
+        input_table = gr.DataFrame(headers=None)
+
+        column_output = gr.DataFrame(label="Selected Column")
+        review_button = gr.Button("総説", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])
+        
+        file_upload.change(fn=lambda file: gr.update(choices=get_column_names(file)), inputs=file_upload, outputs=column_dropdown)
+
+        
+        review_button.click(fn=display_column, inputs=[file_upload, column_dropdown], outputs=[input_table, column_output])
+
+        ####Entity extraction#######
+        prompt=gr.TextArea(label="入力プロンプト",placeholder="please add underscore instead of spaces in column names")
+
+        
+        extract_entity = gr.Button("抽出する", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])
+        
+        gr.Markdown(
+            "<left><span style='font-size: 26px; font-weight: bold;'>生成された出力</span></left>")
+        
+        output=gr.TextArea(label="生成された出力")
+        
+        get_dataframe=gr.Button("get_dataframe", elem_id="custom-button",elem_classes=["custom-button1-class"])
+        
+        output_table=gr.DataFrame(headers=None)
+        
+        ###Button operations ####
+        
+        review_button.click(extract_file.read_table, inputs=file_upload, outputs=input_table)
+        
+        extract_entity.click(extract_file.extract_entity_line_by_line, inputs=[prompt,column_output], outputs=output)
+
+        get_dataframe.click(extract_file.get_table, inputs=[output,file_upload], outputs=output_table)
+        ####Download Utils ####
+        download_button = gr.Button("ダウンロード", elem_id="custom-button1-id",
+                                        elem_classes=["custom-button1-class"])    
+        
+        download_button.click(download_content, inputs=output_table, outputs=gr.File())
 
 demo.launch(server_port=8080)
